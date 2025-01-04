4ea84977027e8f274ae0ad4bc9819d7aaf29dae0
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-04 05:59:33 -0300

    adding users common configuration

---
+++ nixos/common/users/felipemarcelino.nix
@@ -0,0 +1,18 @@
+{ lib, inputs, outputs, }
+{
+    
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.felipemarcelino = {
+    isNormalUser = true;
+    description = "Felipe Glicério Gomes Marcelino";
+    extraGroups = [ "networkmanager" "wheel" "audio" "video"];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+
+    packages = [inputs.home-manager.packages.${pkgs.system}.nix;
+  };
+
+  home-manager.users.felipemarcelino = import ../../../home/felipemarcelino/${config.networking.hostName}.nix; 
+
+}
