a10de67597278c1e80c3d3ac251fbf11a86de34e
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-13 20:27:30 -0300

    conform

--- home-manager/common/nixvim/plugins/default.nix
+++ home-manager/common/nixvim/plugins/default.nix
@@ -3,5 +3,5 @@
   pkgs,
   ...
 }: {
-  imports = [./auto-save.nix ./auto-session.nix ./blink.nix ./better-escape.nix ./cmp.nix ./copilot.nix ./lsp.nix ./treesitter.nix ./trouble.nix];
+  imports = [./auto-save.nix ./auto-session.nix ./blink.nix ./better-escape.nix ./cmp.nix ./conform.nix ./copilot.nix ./lsp.nix ./treesitter.nix ./trouble.nix];
 }
