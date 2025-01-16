3aef701adfcee3cdbe1b5b65e0b565f5ca0f127b
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-16 05:54:14 -0300

    some options related to neovim

--- home-manager/common/nixvim/options.nix
+++ home-manager/common/nixvim/options.nix
@@ -2,7 +2,8 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   programs.nixvim.opts = {
     autoread = true;
     hidden = true;
@@ -19,12 +20,26 @@
     sidescrolloff = 20;
     sidescroll = 5;
     showmode = false;
+    showtabline = 2;
+    lazyredraw = false;
+    synmaxcol = 240;
+    spell = true;
+    cursorcolumn = false;
+    spelllang = "en_us";
+    laststatus = 3;
+    backup = false;
+    modeline = true;
+    modelines = 100;
+    mouse = "a";
+    mousemodel = "extend";
     showmatch = true;
     gdefault = true;
     title = true;
+    cmdheight = 0;
+    copyindent = true;
+    history = 100;
     ch = 3;
     showbreak = "↪ ";
-    iskeyword = "-,_";
     shiftround = true;
     writebackup = false;
     swapfile = false;
@@ -38,10 +53,37 @@
     listchars = "tab:→\ ,eol:¬,trail:⋅,extends:❯,precedes:❮";
     wildmode = "list:longest,list:full";
     foldmethod = "expr";
-    foldnestmax = 10;
-    foldlevelstart = 1;
-    foldlevel = 1;
-    foldenable = false;
+    foldlevelstart = -1;
+    foldlevel = 99;
+    foldenable = true;
     list = true;
+
+    fillchars = {
+      horiz = "━";
+      horizup = "┻";
+      horizdown = "┳";
+      vert = "┃";
+      vertleft = "┫";
+      vertright = "┣";
+      verthoriz = "╋";
+
+      eob = " ";
+      diff = "╱";
+
+      fold = " ";
+      foldopen = "";
+      foldclose = "";
+
+      msgsep = "‾";
+    };
+
+    completeopt = [
+      "menu"
+      "menuone"
+      "noselect"
+    ];
+    pumblend = 10;
+    pumheight = 10;
+    pumwidth = 20;
   };
 }
