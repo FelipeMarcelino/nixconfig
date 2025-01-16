a708c453fd416670aa2cd6f1025764325aec6409
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-16 11:31:58 -0300

    auto session restore

--- home-manager/common/nixvim/plugins/auto-session.nix
+++ home-manager/common/nixvim/plugins/auto-session.nix
@@ -2,17 +2,24 @@
   pkgs,
   config,
   ...
-}: {
+}:
+{
   programs.nixvim.plugins.auto-session = {
     settings.log_level = "info";
-    settings.auto_restore = false;
+    settings.auto_restore = true;
     settings.auto_save = true;
     settings.auto_session = {
       enabled = true;
       enable_last_session = false;
       use_git_branch = true;
       create_enabled = true;
-      suppress_dirs = ["~/.cache" "~/.local/share" "/tmp" "/var/tmp" "~/" "~/Projects"];
+      suppress_dirs = [
+        "~/.cache"
+        "~/.local/share"
+        "/tmp"
+        "/var/tmp"
+        "~/Projects"
+      ];
       root_dir = "${config.home.homeDirectory}/Sessions/";
     };
     settings.session_lens.load_on_setup = true;
