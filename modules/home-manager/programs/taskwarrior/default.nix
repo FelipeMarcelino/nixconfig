e903ec6104e434c43f63512deca1bf26a3dfa406
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-21 04:26:55 -0300

    taskwarrior transitioning some shurtcuts

--- modules/home-manager/programs/taskwarrior/default.nix
+++ modules/home-manager/programs/taskwarrior/default.nix
@@ -20,8 +20,12 @@ in
   config = mkIf cfg.enable {
     programs.taskwarrior = {
       enable = true;
-      dataLocation = "$TASKWARRIOR_DIR";
+      package = pkgs.taskwarrior3;
+      dataLocation = "$XDG_TASKWARRIOR_DIR";
       colorTheme = ./taskwarrior-catppuccin-theme.theme;
+      config = {
+        weekstart = "monday";
+      };
     };
 
     home.packages = with pkgs; [
@@ -31,6 +35,38 @@ in
     systemd.user.tmpfiles.rules = [
       "d ${config.home.homeDirectory}/Taskwarrior/ 0755 felipemarcelino wheel - -"
     ];
+
+    programs.zsh.shellAliases = {
+      rnd = "task add +rnd +next +@computer +@online";
+    };
+
+    programs.zsh.initExtra = ''
+
+      tickle() {
+        deadline=$1
+        shift
+        task add +in wait:$deadline "$@"
+      }
+
+      alias tick="tickle"
+      alias think='tickle +1d'
+
+      webpage_title (){
+        wget -qO- "$\*" | hxselect -s '\n' -c 'title' 2>/dev/null
+      }
+
+      read_and_review (){
+        link="$1"
+        title=$(webpage_title $link)
+        echo $title
+        descr="\"Read and review: $title\""
+        id=$(task add +next +rnr "$descr" | sed -n 's/Created task \(.\*\)./\1/p')
+        task "$id" annotate "$link"
+      }
+
+      alias rnr=read_and_review
+    '';
+
   };
 
 }
