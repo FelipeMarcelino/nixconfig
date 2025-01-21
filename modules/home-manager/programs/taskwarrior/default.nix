0dab24451648dab1d8396d5140828aad83e66b81
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-21 08:17:40 -0300

    taskwarrior remove-in executed

--- modules/home-manager/programs/taskwarrior/default.nix
+++ modules/home-manager/programs/taskwarrior/default.nix
@@ -25,11 +25,44 @@ in
       colorTheme = ./taskwarrior-catppuccin-theme.theme;
       config = {
         weekstart = "monday";
+
+        urgency.user.tag."in".coefficient = 15.0;
+        report."in".columns = "id,description";
+        report."in".description = "Inbox";
+        report."in".filter = "status:pending limit:page (+in)";
+        report."in".labels = "ID,Description";
+
+        uda.brainpower.type = "string";
+        uda.brainpower.label = "Brainpower";
+        uda.brainpower.values = "H,M,L";
+
+        report.next.description = "Most urgent tasks";
+        report.next.columns = "id,start.age,entry.age,urgency,priority,depends,project,tags,recur,scheduled.countdown,due.remaining,until.remaining,description,brainpower";
+        report.next.filter = "status:pending and +next";
+        report.next.labels = "ID, Active, Age, Urgency,Deps,P,Project,Tag,Recur,S,Due, Until,Description,Brainpower";
+        report.next.sort = "brainpower-,priority-,urgency-";
+
+        uda.estimate.type = "numeric";
+        uda.estimate.label = "Estimate";
+
+        context.work = "@work";
+        context.home = "-@work";
+        context.computer = "@computer";
+        context.dosomethingout = "-@computer";
+        context.workoutside = "@work -@computer";
+        context.partner = "@isabel";
+
+        uda.taskwarrior-tui.shortcuts."1" = "remove-in";
+        uda.taskwarrior-tui.keyconfig.shortcut1 = "i";
       };
     };
 
     home.packages = with pkgs; [
       taskwarrior-tui
+      html-xml-utils
+      (writeShellScriptBin "remove-in" ''
+        ${pkgs.taskwarrior3}/bin/task "$@"  modify -in
+      '')
     ];
 
     systemd.user.tmpfiles.rules = [
@@ -52,15 +85,16 @@ in
       alias think='tickle +1d'
 
       webpage_title (){
-        wget -qO- "$\*" | hxselect -s '\n' -c 'title' 2>/dev/null
+        echo "Fetching URL: $1" >&2  # Print the URL being fetched
+        wget -qO- "$1" | hxselect -s '\n' -c 'title' 2>/dev/null
       }
 
       read_and_review (){
         link="$1"
-        title=$(webpage_title $link)
-        echo $title
+        title=$(webpage_title "$link")
+        echo "$title"
         descr="\"Read and review: $title\""
-        id=$(task add +next +rnr "$descr" | sed -n 's/Created task \(.\*\)./\1/p')
+        id=$(task add +next +rnr "$descr" | sed -n 's/Created task \(.*\)./\1/p')
         task "$id" annotate "$link"
       }
 
