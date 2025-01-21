a166f170e371d17e97a5d68dea77ae06d25dc129
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-01-21 08:21:15 -0300

    adding task count into shell

--- modules/home-manager/cli/starship.nix
+++ modules/home-manager/cli/starship.nix
@@ -4,16 +4,30 @@
   pkgs,
   ...
 }:
-with lib; let
-  cfg = config.home.cli.starship or {enable = false;};
-in {
+with lib;
+let
+  cfg = config.home.cli.starship or { enable = false; };
+in
+{
   options.home.cli.starship = mkOption {
     description = "Enable starship for shell";
     type = types.attrs;
-    default = {enable = false;};
+    default = {
+      enable = false;
+    };
   };
 
   config = mkIf cfg.enable {
     programs.starship.enable = true;
+    settings = {
+      custom = {
+        taskin = {
+          command = "task +in +PENDING count";
+          symbol = " ";
+          when = true;
+          shell = "zsh";
+        };
+      };
+    };
   };
 }
